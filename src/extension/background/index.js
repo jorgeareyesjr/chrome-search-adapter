ee94237ebb30d515a4fb5229de4baf57438bab9f
Jorge A. Reyes Jr <reyes.junior.27@gmail.com>
2020-07-25 20:44:18 -0700

    Import 'chrome-extension-async' to the background script (will allow async/await function syntax); Create the extension's 'contextMenu'.

--- src/extension/background/index.js
+++ src/extension/background/index.js
@@ -1,1 +1,13 @@
-console.log("Init background script.");
+import 'chrome-extension-async';
+
+import {
+  createMenu
+} from './utils';
+
+chrome.runtime.onInstalled.addListener(async () => {
+  try {
+    await createMenu();
+  } catch (error) {
+    console.log(error);
+  }
+});
