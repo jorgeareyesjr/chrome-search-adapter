70bbe90be49d29da0f724d5be66140af92ef0d3b
Jorge A. Reyes Jr <reyes.junior.27@gmail.com>
2020-07-31 12:45:34 -0700

    Add base logic for rendering the extension's window UI; Add TODO comments.

--- src/extension/window/index.js
+++ src/extension/window/index.js
@@ -1,1 +1,20 @@
-console.log("Init window script.");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const main = document.createElement('div');
+document.body.appendChild(main);
+
+(async () => {
+  // TODO: Handshake connection with extension's `background script`.
+  // TODO: Handshake connection with extension's `content script`.
+  console.log("windowScript INIT");
+  
+  try {
+    ReactDOM.render(
+      <div>Window UI</div>,
+      main
+    );
+  } catch (err) {
+    console.error(chrome.runtime.lastError);
+  };
+})();
