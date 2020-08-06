b361f9f2b1d3b7f0aec0634147f86948a11c75f1
Jorge A. Reyes Jr <reyes.junior.27@gmail.com>
2020-08-05 19:26:23 -0700

    Update 'App' to a functional component.

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
-class App extends React.Component {
-  render() {
-    return <div>Hello, World!</div>
-  }
+
+function App() {
+  return (
+    <div>Hello, World!</div>
+  );
 };
 
 render(
-  <App />,
+    <App />,
   document.getElementById('o-app-root')
 );
