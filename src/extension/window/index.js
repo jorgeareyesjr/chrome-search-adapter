cf4bd03a9c503b8303b85e28a13dd802b9af862d
Jorge A. Reyes Jr <reyes.junior.27@gmail.com>
2020-08-05 19:49:19 -0700

    Add a redux store to the extension's window script; Add event listeners for updating the user's 'active context' (the active URL); Import the 'adapter'.

--- src/extension/window/index.js
+++ src/extension/window/index.js
@@ -1,20 +1,68 @@
+import 'chrome-extension-async';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import actions from './actions';
+import reducer from './reducer';
+import { render } from 'react-dom';
+import Adapter from './Adapter';
+import * as utils from './utils';
 
+const store = createStore(reducer);
+// store.subscribe(async () => {
+//   await console.log('state\n', store.getState());
+// });
+
+/** 
+ * This section is in charge of monitoring URL changes. 
+ * SEE: https://developer.chrome.com/extensions/tabs
+*/
+
+// Detect when a tab is updated.
+// NOTE: This will automatically activate after a new tab or window is created.
+chrome.tabs.onUpdated.addListener(async (tabId, { status, url }, tab) => {
+  // NOTE: The `url` parameter will only return a value if the tab's URL has changed, otherwise it will return `undefined`.
+  try {
+    if (status === 'complete') {
+      const activeTab = await chrome.tabs.get(tabId);
+      const { id, url } = activeTab;
+
+      if (activeTab.url) {
+        await utils.setActiveContext(id, url);
+      };
+    }
+  } catch (error) {
+    console.log(error);
+  };
+});
+
+// Detect when the active tab in a window changes.
+// NOTE: This will automatically activate after a tab is attached/detached from a window.
+chrome.tabs.onActivated.addListener(async ({ tabId }) => {
+  try {
+    const activeTab = await chrome.tabs.get(tabId);
+    const { id, url } = activeTab;
+
+    if (activeTab.url) {
+      await utils.setActiveContext(id, url);
+    };
+  } catch (error) {
+    console.log(error)
+  };
+});
+
+utils.connectToBackgroundScript();
+
+
+// Initialize the extension adapter.
 const main = document.createElement('div');
 document.body.appendChild(main);
 
-(async () => {
-  // TODO: Handshake connection with extension's `background script`.
-  // TODO: Handshake connection with extension's `content script`.
-  console.log("windowScript INIT");
-  
-  try {
-    ReactDOM.render(
-      <div>Window UI</div>,
-      main
-    );
-  } catch (err) {
-    console.error(chrome.runtime.lastError);
-  };
-})();
+render(
+  <Provider store={store}>
+    <Adapter />
+  </Provider>,
+  main
+);
+
+export { store };
