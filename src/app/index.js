e918b9ae5c21d29d6015bb64dc0d7dae6b0595b2
Jorge A. Reyes Jr <reyes.junior.27@gmail.com>
2020-08-08 10:19:16 -0700

    Add the new '__CHROME_WEB_BROWSER_ADAPTER__' to 'app' and update the UI.

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { render } from 'react-dom';
 
 function App() {
-  return (
-    <div>Hello, World!</div>
-  );
+  const { activeBrowserTabUrl, extensionWindowId } = window.__CHROME_WEB_BROWSER_ADAPTER__;
+
+  if(activeBrowserTabUrl) {
+    return (
+      <Fragment>
+        <h1>Hello, World!</h1>
+        <p>{`I am Chrome Extension Adapter: ${extensionWindowId}.`}</p>
+        <p>{`The current browser context is: ${activeBrowserTabUrl}.`}</p>
+      </Fragment>
+    );
+  } else {
+    return null;
+  };
 };
 
 render(
-    <App />,
+  <App />,
   document.getElementById('o-app-root')
 );
