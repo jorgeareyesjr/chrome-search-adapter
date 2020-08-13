dea5d1c5da1151640258ca6f22a294df16b10988
Jorge A. Reyes Jr <reyes.junior.27@gmail.com>
2020-08-12 20:25:18 -0700

    Give the content script the ability to establish a long-lived connection to the extension adapter's window script.

--- src/extension/content/index.js
+++ src/extension/content/index.js
@@ -1,3 +1,27 @@
-// TODO: Handshake connection with extension's `background script`.
-// TODO: Handshake connection with extension's `window script`.
-console.log("Init content script.");
+/**
+ * Connect to the extension adapter's window script, whenever the content script is successfully injected.
+ * @param {object} port - The connection channel used to pass messages between two separate scripts running in different browsing contexts - In this case, the extension window script and injected content script.
+ * SEE: https://developer.chrome.com/extensions/runtime#event-onConnect
+ */
+async function connectToAdapterWindow(port) {
+  const handleDisconnect = () => {
+    port.onMessage.removeListener(handleMessages);
+    chrome.runtime.onConnect.removeListener(connectToAdapterWindow)
+  };
+  const handleMessages = async (message) => {
+    switch (message.type) {
+      case "TEST CONNECTION": {
+        console.log('CONNECTED TO WINDOW SCRIPT.');
+        port.postMessage({
+          type: "TEST CONNECTION SUCCESS"
+        });      
+        break;
+      };
+    };
+  };
+
+  port.onMessage.addListener(handleMessages);
+  port.onDisconnect.addListener(handleDisconnect);
+};
+
+chrome.runtime.onConnect.addListener(connectToAdapterWindow);
