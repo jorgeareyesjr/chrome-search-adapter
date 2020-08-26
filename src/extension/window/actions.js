5438728f5609e3c01b2a85a51c799d03810860a5
Jorge A. Reyes Jr <reyes.junior.27@gmail.com>
2020-08-25 20:38:53 -0700

    Update the actions; Add new functions to extract any relevant DOM data.

--- src/extension/window/actions.js
+++ src/extension/window/actions.js
@@ -1,15 +1,29 @@
 const actions = {
+  CLEAR_DOM_DATA: "CLEAR_DOM_DATA",
   SET_ACTIVE_BROWSER_TAB_ID: 'SET_ACTIVE_TAB_ID',
   SET_ACTIVE_BROWSER_TAB_URL: 'SET_ACTIVE_TAB_URL',
   SET_ACTIVE_BROWSER_WINDOW_ID: 'SET_ACTIVE_WINDOW_ID',
+  SET_DOM_FLIGHT_DESTINATION: 'SET_DOM_FLIGHT_DESTINATION',
+  SET_DOM_FLIGHT_ORIGIN: 'SET_DOM_FLIGHT_ORIGIN',
+  SET_DOM_SEARCH_INPUT_TERM: "SET_DOM_SEARCH_INPUT_TERM",
   SET_DOM_SEARCH_RESULTS: 'SET_DOM_SEARCH_RESULTS',
+  SET_DOM_SELECTED_FLIGHTS: 'SET_DOM_SELECTED_FLIGHTS',
   SET_EXTENSION_WINDOW_ID: 'SET_EXTENSION_WINDOW_ID',
+  SET_PAGETYPE: "SET_PAGETYPE",
   SET_PARENT_TAB_ID: 'SET_PARENT_TAB_ID',
   SET_PARENT_WINDOW_ID: 'SET_PARENT_WINDOW_ID',
   SET_SUPPORTED_URLS: 'SET_SUPPORTED_URLS',
 };
 
 const actionCreators = {
+  clearDOMData(value) {
+    return {
+      type: actions.CLEAR_DOM_DATA,
+      payload: {
+        value
+      }
+    };
+  },
   setActiveBrowserTabId(tabId) {
     return {
       type: actions.SET_ACTIVE_BROWSER_TAB_ID,
@@ -34,9 +48,41 @@ const actionCreators = {
       }
     };
   },
+  setDOMFlightDestination(DOMElement) {
+    return {
+      type: actions.SET_DOM_FLIGHT_DESTINATION,
+      payload: {
+        DOMElement
+      }
+    };
+  },
+  setDOMFlightOrigin(DOMElement) {
+    return {
+      type: actions.SET_DOM_FLIGHT_ORIGIN,
+      payload: {
+        DOMElement
+      }
+    };
+  },
+  setDOMSearchInputTerm(DOMElement) {
+    return {
+      type: actions.SET_DOM_SEARCH_INPUT_TERM,
+      payload: {
+        DOMElement
+      }
+    };
+  },
   setDOMSearchResults(DOMElements) {
     return {
       type: actions.SET_DOM_SEARCH_RESULTS,
+      payload: {
+        DOMElements
+      }
+    };
+  },
+  setDOMSelectedFlights(DOMElements) {
+    return {
+      type: actions.SET_DOM_SELECTED_FLIGHTS,
       payload: {
         DOMElements
       }
@@ -47,6 +93,14 @@ const actionCreators = {
       type: actions.SET_EXTENSION_WINDOW_ID,
       payload: {
         windowId
+      }
+    };
+  },
+  setPageType(pageType) {
+    return {
+      type: actions.SET_PAGETYPE,
+      payload: {
+        pageType
       }
     };
   },
